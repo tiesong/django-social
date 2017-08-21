b9bdce640789eb39f69ec4c909849db538670ea2
salient <owner@gt.com>
2017-08-22 02:33:05 +0300

    implement backend of officespace

--- officespace/admin.py
+++ officespace/admin.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Room, Booking
 
 # Register your models here.
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'category',)
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'owner', 'booked_date', 'time_start', 'time_end',)
--- officespace/models.py
+++ officespace/models.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+ROOM_TYPE = [('meeting_space', 'Meeting Space'), ('releconference_room', 'Teleconference Room'), ('workspaces', 'Workspaces'), ('misc', 'Misc')]
+class Room(models.Model):
+    name = models.CharField(max_length=150,)
+    category = models.CharField(max_length=100, choices=ROOM_TYPE, default='Meeting Space')
+
+    def __str__(self):
+        return self.name
+
+class Booking(models.Model):
+    room = models.ForeignKey(Room)
+    owner = models.ForeignKey(User)
+    booked_date = models.DateField('date booked')
+    time_start = models.TimeField('start time')
+    time_end = models.TimeField('end time')
+
+    def __str__(self):
+    	return str(self.room)
