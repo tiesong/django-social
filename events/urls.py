f216cfc27d79adb7224e8390eb29f10d39db4a8c
Wenwei Lin <youdontseemehaha@gmail.com>
2017-08-22 16:32:46 -0700

    working on event.

--- events/urls.py
+++ events/urls.py
@@ -4,6 +4,7 @@ from events import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^create/$', views.create, name='create'),
     # ex: /polls/5/
     url(r'^(?P<event_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- events/views.py
+++ events/views.py
@@ -6,38 +6,62 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
 from datetime import datetime, timedelta
 
-#from .models import Event
+# from .models import Event
 from .models import Event
+
 
 # Create your views here.
 
 @login_required
 def index(request):
-	
-	try:
-		week=int(request.GET.get('week', ''))
-		next_week=week+1
-		previous_week=week-1
-	except:
-		week=0
-		next_week=1
-		previous_week=-1
+    try:
+        week = int(request.GET.get('week', ''))
+        next_week = week + 1
+        previous_week = week - 1
 
-	#this sets up for a 10 day event view window in the template
-	base_date = datetime.now()+timedelta(week*10)
-	limit_date = datetime.now()+timedelta(10+week*10)
-	event_list = Event.objects.filter(start_date__gte=base_date).filter(start_date__lte=limit_date).order_by('start_date')
+    except:
+        week = 0
+        next_week = 1
+        previous_week = -1
 
-	context = {
-	'next_week': next_week,
-	'previous_week': previous_week,
-	'event_list': event_list
-	}
-	return render(request, 'events/event-list.html', context)
+    # this sets up for a 10 day event view window in the template
+    base_date = datetime.now() + timedelta(week * 10)
+    limit_date = datetime.now() + timedelta(10 + week * 10)
+    event_list = Event.objects.filter(start_date__gte=base_date).filter(start_date__lte=limit_date).order_by(
+        'start_date')
 
+    context = {
+        'next_week': next_week,
+        'previous_week': previous_week,
+        'event_list': event_list
+    }
+    return render(request, 'events/event-list.html', context)
+
+
+@login_required
 def detail(request, event_id):
-	event = Event.objects.get(id=event_id)
-	context={
-	'event': event,
-	}
-	return render(request, 'events/event-details.html', context)
+    event = Event.objects.get(id=event_id)
+    context = {
+        'event': event,
+    }
+    return render(request, 'events/event-details.html', context)
+
+
+@login_required
+def create(request):
+    """
+    Create new event.
+    :param request: 
+    :return: 
+    """
+    if request.POST:
+
+        title = request.POST["title"]
+        start_time = request.POST["start-time"]
+        start_date = request.POST["start-date"]
+        pub_time = request.POST["end-time"]
+        pub_date = request.POST["end-date"]
+        feature_image = request.POST["featureImage"]
+        event_url = request.POST["event-url"]
+
+    return render(request, 'events/event-edit.html')
