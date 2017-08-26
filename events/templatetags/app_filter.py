6f8e8a29038519aaefc7cbe684ee4568742cd7cc
Wenwei Lin <youdontseemehaha@gmail.com>
2017-08-25 22:47:50 -0700

    heroku collectstatic disable==1

--- events/templatetags/app_filter.py
+++ events/templatetags/app_filter.py
@@ -8,6 +8,23 @@ import re
 register = template.Library()
 
 
+@register.filter(name="snippet")
+def snippet(html_body):
+    """
+    Return snippet from html body
+    :param html_body: 
+    :return: 
+    """
+    snippet_txt = ""
+    soup = BeautifulSoup(html_body, "html.parser")
+
+    for p in soup.select("p"):
+
+        snippet_txt += p.text
+
+    return snippet_txt[:150]
+
+
 @register.filter(name="fulltext")
 def fulltext(html_body):
     """
