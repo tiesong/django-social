56b4a41d9d94aa65450654939bf50da0e7a35282
Wenwei Lin <youdontseemehaha@gmail.com>
2017-08-27 07:31:40 -0700

    heroku collectstatic disable==1

--- events/views.py
+++ events/views.py
@@ -44,7 +44,7 @@ def index(request):
 @login_required
 def detail(request, event_id):
     event = Event.objects.get(id=event_id)
-    print ('event: {}'.format(event))
+    print ('event detail: {}'.format(event.detail))
     context = {
         'event': event,
     }
