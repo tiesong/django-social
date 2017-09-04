53f79700b131d8e63a689cae3a1d42be4ef07f6c
Wenwei Lin <youdontseemehaha@gmail.com>
2017-09-04 12:57:31 -0700

    update news page.

--- news/urls.py
+++ news/urls.py
@@ -5,6 +5,10 @@ from news import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^create/$', views.create, name='create'),
+    url(r'^update/$', views.update, name='update'),
+    url(r'^category/$', views.category, name='category'),
+    url(r'^search/$', views.search, name='search'),
+    # url(r'^search/(?P<keyword>.+)/$', views.search, name='search'),
     # ex: /polls/5/
     url(r'^(?P<news_article_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<news_article_id>[0-9]+)/edit$', views.edit, name='edit'),
