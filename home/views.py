b17dfedcff27ccd96fda5fdd90da32c1de8ef8ef
nick byrne <nick@thinkactlive.com>
2017-09-04 23:54:03 +1000

    added basic signup and authentication exception handling

--- home/views.py
+++ home/views.py
@@ -15,30 +15,30 @@ def index(request):
         feature_list = False
 
     email = password = ''
+
+
     if request.POST:
+        #collect all potential field inputs
         logout(request)
         email = request.POST['login-email']
         password = request.POST['login-password']
 
-        try:
+        try: #we try logging a user in first
             username = User.objects.get(email=email.lower()).username
-        except:
-            #no username found in system
-            return HttpResponseRedirect('/error')
-
-        try:
             user = authenticate(username=username, password=password)
-        except:
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/news/')
+                else:
+                    #user is not in the system
+                    message = "Sorry, but we could not find your username in our system. Please try again."
+                    request.session['error'] = message
+                    return HttpResponseRedirect('/error')
+        
+        except: #if logging in fails, let's try signing them up
+            request.session['error'] = "Sorry, the username / password combination could not be found. Please try again."
             return HttpResponseRedirect('/error')
-
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return HttpResponseRedirect('/news/')
-        else:
-            #user is not in the system
-            return HttpResponseRedirect('/error')
-
   
     context = {
         'feature_list': feature_list,
@@ -46,8 +46,31 @@ def index(request):
 
     return render(request, 'home/index.html', context)
 
+def signup(request):
+    try:
+        if request.POST:
+            signup_firstname = request.POST['signup-firstname']
+            signup_lastname = request.POST['signup-lastname']
+            signup_email = request.POST['signup-email']
+            signup_password = request.POST['signup-password']
+
+            user = User.objects.create_user(username=signup_email, first_name=signup_firstname, last_name=signup_lastname)
+            login(request, user)
+            return HttpResponseRedirect('/news/')
+        else:
+            return HttpResponseRedirect('/error')
+    except: #if logging in fails, let's try signing them up
+        return HttpResponseRedirect('/error')
+
 def error(request):
-    message = "Sorry, your username and/or password were incorrect. Please go back and try again."
+
+    try:
+        message = request.session['error']
+        request.session['error'] = ''
+    except:
+        message = "Whoops. Something has gone wrong. Please try again or email nick@typehuman.com for help."
+        request.session['error'] = ''
+    
     context = {
         'message': message,
     }
--- ybf-teamedup/urls.py
+++ ybf-teamedup/urls.py
@@ -16,6 +16,7 @@ import home.views
 urlpatterns = [
     url(r'^$', home.views.index, name='index'),
     url(r'^error/', home.views.error, name='error'),
+    url(r'^signup/', home.views.signup, name='signup'),
 	url(r'^tinymce/', include('tinymce.urls')),
     url(r'^news/', include('news.urls')),
     url(r'^c/', include('community.urls')),
