0b69d08245cc8e90c1887afa8f6875c10b4212bd
salient <owner@gt.com>
2017-09-13 07:59:59 +0800

    add MEDIA_ROOT in settings.py

--- ybf-teamedup/settings.py
+++ ybf-teamedup/settings.py
@@ -163,6 +163,7 @@ NOTIFICATIONS_USE_JSONFIELD=True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
 APP_Filters = (os.path.join(PROJECT_ROOT, 'templatetags'))
 
