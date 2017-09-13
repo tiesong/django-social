cb988c8943522722b5fe2230ca939ef45e5aa7ca
Wenwei Lin <youdontseemehaha@gmail.com>
2017-09-12 18:14:08 -0700

    change url name on perks

--- perks/urls.py
+++ perks/urls.py
@@ -5,9 +5,9 @@ from perks import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^create/$', views.create, name='create'),
-    url(r'^update/$', views.update, name='updateNews'),
-    url(r'^category/$', views.category, name='categoryNews'),
-    url(r'^search/$', views.search, name='searchNews'),
+    url(r'^update/$', views.update, name='updatePerks'),
+    url(r'^category/$', views.category, name='categoryPerks'),
+    url(r'^search/$', views.search, name='searchPerks'),
     # url(r'^search/(?P<keyword>.+)/$', views.search, name='search'),
     # ex: /polls/5/
     url(r'^(?P<news_article_id>[0-9]+)/$', views.detail, name='detail'),
