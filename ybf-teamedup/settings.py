2178d71a16641657d377c34364fa853557a5668e
salient <owner@gt.com>
2017-09-14 12:27:13 +0800

    fix settings.py

--- ybf-teamedup/settings.py
+++ ybf-teamedup/settings.py
@@ -169,7 +169,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 APP_Filters = (os.path.join(PROJECT_ROOT, 'templatetags'))
 
 # S3 bucket serving media files.
-if os.environ['DJANGO_DEVELOPMENT'] == 'PRODUCTION':
+if os.environ['DJANGO_DEVELOPMENT'] != 'LOCAL':
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
