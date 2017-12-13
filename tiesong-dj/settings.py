1d2963c6ba68c03db2525f8f441d27e78d183a25
tiesongliu <tiesongliu@mail.com>
2017-12-13 16:11:10 +0800

    adding staticfiles

--- tiesong-dj/settings.py
+++ tiesong-dj/settings.py
@@ -166,7 +166,7 @@ NOTIFICATIONS_USE_JSONFIELD=True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '../tiesong-dj/staticfiles')
 
 APP_Filters = (os.path.join(PROJECT_ROOT, 'templatetags'))
 
